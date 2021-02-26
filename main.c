ac94c951058dd61b271241b35c556f7aabb02523
Adrian-Rochminski <79656651+Adrian-Rochminski@users.noreply.github.com>
2021-02-27 00:47:21 +0100

    01 Zmiana metody Policz

--- main.c
+++ main.c
@@ -5,7 +5,9 @@ int Policz(int a, int b);
 
 int main() {
     printf("Hello, World!\n");
-    printf("W pierszym branchu 2+3= %d", Policz(2,3));
+    printf("W pierszym branchu 2+3= %d\n", Policz(2,3));
+    printf("zmienione");
+
     return 0;
 }
 
