ff024d1e16a53a34ff62b1b03f61fe9f42329fb9
Adrian-Rochminski <79656651+Adrian-Rochminski@users.noreply.github.com>
2021-02-27 00:38:35 +0100

    Initial commit proba 1

---
+++ cmake-build-debug/CMakeFiles/git_zadanie.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/git_zadanie.dir/main.c.obj"
+  "git_zadanie.exe"
+  "git_zadanie.exe.manifest"
+  "git_zadanie.pdb"
+  "libgit_zadanie.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/git_zadanie.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/git_zadanie.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for git_zadanie.
+# This may be replaced when dependencies are built.
