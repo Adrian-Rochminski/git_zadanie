ed1b0cca0cfc6f5964e1fcb5d6bda0e38c7c6244
Adrian-Rochminski <79656651+Adrian-Rochminski@users.noreply.github.com>
2021-02-27 00:45:07 +0100

    01 Zmiana metody Policz

--- main.c
+++ main.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 
+int Policz(int a, int b);
+
+
 int main() {
     printf("Hello, World!\n");
+    printf("W pierszym branchu 2+3= %d", Policz(2,3));
     return 0;
+}
+
+
+int Policz(int a, int b){
+
+    return a+b;
+
 }
